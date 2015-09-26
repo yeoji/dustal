bff9af035127bebd5ff8d23260a68752cf68b55a
Joey Lee <jayd005@gmail.com>
2015-09-26 12:55:52 +1000

    wip

--- app/database/mongo/models/Comment.js
+++ app/database/mongo/models/Comment.js
@@ -1,6 +1,7 @@
 export default class Comment {
     static register(mongoose) {
         let CommentSchema = new mongoose.Schema({
+            post_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Post'},
             user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
             message: {type: String, default: ''},
             timestamp: {type: Date, default: Date.now}
